7efdf3dab0755b6f50085d03d0347c939eeb9643
didu <yashdeepsingh@hotmail.com>
2018-08-09 10:14:42 +0000

    lab4_1 added

---
+++ lab4_1.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+class Employee
+{
+	long empcode;
+	double salary;
+	char *name;
+
+public:Employee(long empcode, double salary, char *str)
+{
+	this->empcode = empcode;
+	this->salary = salary;
+	this->name = str;
+}
+	   long getEmpcode()
+	   {
+		   return empcode;
+	   }
+	   double getsalary()
+	   {
+		   return salary;
+	   }
+	   char * getName()
+	   {
+		   return name;
+	   }
+
+	   void displayDeatils()
+	   {
+		   cout << "Employee Code : " << getEmpcode() << "\nSalary : " << getsalary() << "\nName : " << getName();
+
+	   }
+	   ~Employee()
+	   {
+
+	   }
+};
+int main()
+{
+	Employee e(101, 20000, "Rashmi");
+
+	e.displayDeatils();
+
+	return 0;
+}
