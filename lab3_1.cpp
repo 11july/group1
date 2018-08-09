768033afe1c6beb6b6c2c118a5c7749125f666f2
didu <yashdeepsingh@hotmail.com>
2018-08-09 09:45:41 +0000

    lab3 added

---
+++ lab3_1.cpp
@@ -0,0 +1,41 @@
+/*
+Class Diagram:
+
+----------------Student----------------------
+-iRoll:int                                  -
+-cName:char[10]                             -
+-fPercentage: float                         -
+---------------------------------------------
++setStudent(int,char[],float):void          -
++ getStudent(void):void                     -
+---------------------------------------------
+*/
+
+#include <iostream> 
+using namespace std;
+class Student{
+	int iRoll;
+	char cName[30];
+	float fPercentage;
+public:
+	void setStudent(int isetRoll, char csetName[], float fsetPercentage) 
+	{
+	}
+	void getStudent(void) {
+	}
+};
+void main()
+{
+	Student stud[10];
+	char cStudName[30];
+	int iStudRoll;
+	float fStudPercentage;
+	for (int iCounter = 0; iCounter<10; iCounter++) {
+		//Accept data for 10 students 
+	}
+	cout << "The students Scoring First class are " << endl;
+	for (int iIndex = 0; iIndex<10; iIndex++) {
+		//Display data of the students scoring more than 60 % only 
+		cout << "***************************************";
+	}
+}
